a235f353befe7543275a3901e2a58a77ae300487
Roberto Gian Franco Carbonell Saravia <r.gian.f.carbonell.s@gmail.com>
2016-11-16 09:27:46 -0500

    qr

--- empadronados/urls.py
+++ empadronados/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^empadronado/nuevo/$', views.EmpadronadoCreateView.as_view(), name='create'),
+    url(r'^verificar/empadronado/$', views.EmpadronadoSearchQRListView.as_view(), name='codigo_barra_qr'),
     url(r'^empadronado/(?P<slug>[-\w\W\d]+)/modificar/$', views.EmpadronadoUpdateView.as_view(), name='update'),
     url(r'^empadronados/$', views.EmpadronadoControlListView.as_view(), name='control'),
     url(r'^empadronados/(?P<search_registro>[-\w\W\d]+)/$', views.EmpadronadoControlListView.as_view(), name='control'),
@@ -11,6 +12,5 @@ urlpatterns = [
     url(r'^empadronado/(?P<slug>[-\w\W\d]+)/imprimir$', views.EmpadronadoPrintTicketDetailView.as_view(), name='print'),
     url(r'^reportes$', views.EmpadronadoReportListView.as_view(), name='report'),
 
-    url(r'^verificar/empadronado/$', views.EmpadronadoSearchQRListView.as_view(), name='codigo_barra_qr'),
 
 ]
